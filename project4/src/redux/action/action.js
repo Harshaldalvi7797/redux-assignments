1393b4896246ff58c7547ad1bebc537b73923b0d
Harshaldalvi7797 <[dalviharshal7797@gmail.com]>
2020-02-13 13:54:45 +0530

    comiit: update response

--- project4/src/redux/action/action.js
+++ project4/src/redux/action/action.js
@@ -4,7 +4,9 @@ export const fetchPostList =  ()=>
     return async (dispatch) =>
     {
         let response = await axios.get("https://jsonplaceholder.typicode.com/posts");
-        // console.log(response);
+
+
+        console.log(response);
 
         dispatch({type: "FETCH_LIST", payload: response.data})
 
